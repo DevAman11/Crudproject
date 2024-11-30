82f0817d5419d002bbd3147956d745433da88137
DevAman11 <aman11newus@gmail.com>
2024-11-30 15:00:22 +0530

    update

--- Nodejs Crud App/FrontEnd/src/assets/Components/SignUp.jsx
+++ Nodejs Crud App/FrontEnd/src/assets/Components/SignUp.jsx
@@ -1,44 +1,112 @@
-import React from 'react'
-import {Link, useState} from "react-router-dom"
-
+import React from "react";
+import { Await, Link, useState } from "react-router-dom";
 
 function SignUp() {
-   const [Form,setForm] = useState({
-      Name:'',
-      
-   })
-   
+  const [Form, setForm] = useState({
+    Name: "",
+    Phone: "",
+    Email: "",
+    Password: "",
+  });
+
+  const HandleValueChange = (e) => {
+    setForm({
+      ...Form,
+      [e.target.Name]: e.target.value,
+    });
+  };
+
+  const SubmitValue = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("http://localhost:7000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "appication/json",
+        },
+        body: JSON.stringify(Form),
+      });
+      if (!response.ok) {
+        const errorData = await response.json();
+        alert(`Error:  ${errorData.message}`);
+      } else {
+        const Data = await response.json();
+        setForm({
+          Name: "",
+          Phone: "",
+          Email: "",
+          Password: "",
+        });
+        alert("Signup Successfully", Data);
+      }
+    } catch (error) {
+      console.log(error);
+      alert("An Error occurred while singing up.");
+    }
+  };
+
   return (
-   <>
-   <h1> SignUp Page</h1>
-    <div className='ContainerDiv '>
-        <div className='ParentMain'>
+    <>
+      <h1> SignUp Page</h1>
+      <form>
+        <div className="ContainerDiv ">
+          <div className="ParentMain">
+            <div className="ParentContent">
+              <div className="Content">
+                <div className="HeadingDiv">
+                  <h1>SignUp</h1>
+                </div>
+                <div className="inputDiv">
+                  <input
+                    type="text"
+                    placeholder="Name"
+                    name="Name"
+                    value={Form.Name}
+                    onChange={HandleValueChange}
+                  />{" "}
+                  <br />
+                  <input
+                    type="phone"
+                    placeholder="Phone"
+                    name="Phone"
+                    value={Form.Phone}
+                    onChange={HandleValueChange}
+                  />{" "}
+                  <br />
+                  <input
+                    type="email"
+                    placeholder="Email"
+                    name="Email"
+                    value={Form.Email}
+                    onChange={HandleValueChange}
+                  />{" "}
+                  <br />
+                  <input
+                    type="password"
+                    placeholder="Password"
+                    name="Password"
+                    value={Form.Password}
+                    onChange={HandleValueChange}
+                  />{" "}
+                  <br />
+                </div>
+                <div className="ButtonDiv">
+                  <button type="submit">SignUp</button>
+                </div>
 
-        <div className='ParentContent'> 
-        <div className='Content'> 
-           <div className='HeadingDiv'>
-            <h1>SignUp</h1>
-           </div>
-           <div className='inputDiv'>
-           <input type='text' placeholder='Name'  name='Name'/>  <br/>
-           <input type='phone' placeholder='Phone' name='Phone'/> <br/>
-           <input type='email' placeholder='Email' name='Email'/> <br/>
-           <input type='password' placeholder='Password' name='Password'/> <br/>
+                <p className="mt-10 text-center text-sm/6 text-gray-500">
+               Already Have an Account
+            <Link to="/Login" className="font-semibold text-indigo-600 hover:text-indigo-500">
+           Login
+            </Link>
+          </p>
+              </div>
             </div>
-            <div className='ButtonDiv'>
-
-           <button type='submit'>
-              SignUp
-           </button>
-            </div>
-
-         </div>
+          </div>
         </div>
-       
-        </div>
-    </div>
-   </>
-  )
+      </form>
+    </>
+  );
 }
 
-export default SignUp
+export default SignUp;
--- Nodejs Crud App/FrontEnd/src/assets/Components/Update.jsx
+++ Nodejs Crud App/FrontEnd/src/assets/Components/Update.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 function Update() {
-  return (
-    <div>Update</div>
-  )
+
+  const {id} = useParams()
+  const [Form, setForm] = useState({
+    Name: "",
+    Phone: "",
+    Email: "",
+    Password: "",
+  });
+
+  useEffect( ()=>{
+    const FetchData  = async ()=>{
+      try {
+        const res = await fetch(`http://localhost:7000/login${id}`)
+        const Logtech = await res.json()
+        console.log(Logtech);
+        setForm(Logtech)
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    FetchData()
+  },[id])
+
+  const HandleValueChange = (e) => {
+    setForm({
+      ...Form,
+      [e.target.Name]: e.target.value,
+    });
+  };
+
+  return <>
+
+  </>;
 }
 
-export default Update
+export default Update;
